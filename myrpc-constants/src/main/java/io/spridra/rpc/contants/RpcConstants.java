32b515255c4ae5afda6dd5d70d9f01a3213c6b9e
chencl <2394527308@qq.com>
2025-05-27 16:23:03 +0800

    feat: 第六 自定义网络传输协议
    常量模块搭建

---
+++ myrpc-constants/src/main/java/io/spridra/rpc/contants/RpcConstants.java
@@ -0,0 +1,15 @@
+package io.spridra.rpc.contants;
+
+/**
+ * @Author: Spridra
+ * @CreateTime: 2025-05-27 16:18
+ * @Describe: 常量类
+ * @Version: 1.0
+ */
+
+public class RpcConstants {
+    /**
+     * 魔数
+     */
+    public static final short MAGIC = 0x10;
+}
